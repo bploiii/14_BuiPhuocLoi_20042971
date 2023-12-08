8c5269c64e934fefda200423075ebe91e6b43b0d
bploiii <phuocloi4102@gmail.com>
2023-12-08 08:37:06 +0700

    Cau02

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import StackTakeNotes from './navigation/StackTakesNote';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer
+    styles={styles.container}
+    >
+      <StackTakeNotes></StackTakeNotes>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FADFE3',
   },
 });
